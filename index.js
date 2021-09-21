7fc7b4687d94b1772170e383ef3a77e724a64dd9
James <jamesschmidt124@gmail.com>
2021-09-21 14:33:05 -0700

    James Driver, Lisa complted first 7 tests

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+//const returnFirstTwoDrivers = (array) => {
+    //return array.slice(0,2);
+//}
+//const returnLastTwoDrivers = (array) => {
+    //return array.slice(2, 4);
+//}
+//const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const returnFirstTwoDrivers = (array) => {
+    return array.slice(0, 2);
+}
+const returnLastTwoDrivers = (array) => {
+    
+    return array.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (integer) => {
+     return () => integer * 5;
+}
